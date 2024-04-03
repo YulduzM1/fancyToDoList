9a9d6989212e0f1cbfd2cae7df7b2f6006e7c9a5
Yulduz Muradova <muradovayulduz1@gmail.com>
2024-04-03 01:08:50 -0400

    added ui for SignUp screen

---
+++ lib/pages/signup_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign up'),
+      ),
+      body: SignupForm(),
+    );
+  }
+}
+
+class SignupForm extends StatefulWidget {
+  @override
+  _SignupFormState createState() => _SignupFormState();
+}
+
+class _SignupFormState extends State<SignupForm> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextField(
+            controller: _emailController,
+            decoration: InputDecoration(labelText: 'Email'),
+          ),
+          TextField(
+            controller: _passwordController,
+            decoration: InputDecoration(labelText: 'Password'),
+            obscureText: true,
+          ),
+          SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              // Implement signUp method here
+            },
+            child: Text('Sign up'),
+          ),
+        ],
+      ),
+    );
+  }
+}
